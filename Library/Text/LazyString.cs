5b90ba6526ffb888f3f7c0e7b5af2bdbb33a7c74
vituha <vituha@d88d2579-543e-0410-9381-c375477b3cb3>
2008-12-25 22:14:03 +0000

    LazyString is introduced. This should be somewhat faster then StringBuilder because it doesn't do re-allocations of result string along the way

---
+++ Library/Text/LazyString.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VS.Library.Text
+{
+    public sealed class LazyString
+    {
+        private List<string> buffer;
+        private string value = String.Empty;
+        public int Length { get; private set; }
+
+        public LazyString() {
+            buffer = new List<string>();
+        }
+
+        public LazyString(int capacity)
+        {
+            buffer = new List<string>(capacity);
+        }
+
+        public override string ToString() {
+            if (value == null)
+            {
+                BuildValue();
+            }
+            return value;
+        }
+
+        public void Clear() {
+            buffer.Clear();
+            Length = 0;
+            value = String.Empty;
+        }
+
+        public bool Append(string item) {
+            if (String.IsNullOrEmpty(item))
+            {
+                return false;
+            }
+            value = null;
+            buffer.Add(item);
+            Length += item.Length;
+            return true;
+        }
+        public bool AppendRange(IEnumerable<string> items) {
+            var modified = false;
+            foreach (var item in items)
+            {
+                if (!String.IsNullOrEmpty(item))
+                {
+                    buffer.Add(item);
+                    Length += item.Length;
+                    modified = true;
+                }
+            }
+            if (modified)
+            {
+                value = null;
+            }
+            return modified;
+        }
+
+        private void BuildValue()
+        {
+            var sb = new StringBuilder(Length);
+            var itemCount = buffer.Count;
+            for (int i = 0; i < itemCount; i++)
+            {
+                sb.Append(buffer[i]);
+            }
+            value = sb.ToString();
+        }
+    }
+}
---
+++ UnitTests/Library/Text/LazyString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace VS.Library.UT.Text
+{
+    [TestFixture]
+    public class LazyString : AssertionHelper
+    {
+        private const int testCount = 1000000;
+        private const int length = 6;
+        private const string s = "##############################################################################";
+
+        [Test]
+        public void TestLazyString() {
+            var ticks = Environment.TickCount;
+            for (int j = 0; j < testCount; j++)
+            {
+                var ls = new VS.Library.Text.LazyString(length);
+                for (int i = 0; i < length; i++)
+                {
+                    ls.Append(s);
+                }
+                var l = ls.ToString().Length;
+            }
+            Console.WriteLine((Environment.TickCount - ticks).ToString());
+        }
+
+        [Test]
+        public void TestStringBuilder()
+        {
+            var ticks = Environment.TickCount;
+            for (int j = 0; j < testCount; j++)
+            {
+                var sb = new StringBuilder();
+                for (int i = 0; i < length; i++)
+                {
+                    sb.Append(s);
+                }
+                var l = sb.ToString().Length;
+            }
+            Console.WriteLine((Environment.TickCount - ticks).ToString());
+        }
+    }
+}
