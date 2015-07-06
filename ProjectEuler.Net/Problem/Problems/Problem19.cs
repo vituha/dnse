ae1ead8e525cb61693030859cb7da4dd3848f412
spam@vituha.net <spam@vituha.net>
2015-07-06 22:27:53 +0300

    Problem 19 solved

---
+++ ProjectEuler.Net/Problem/Problems/Problem19.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problem.Problems
+{
+    internal sealed class Problem19 : ProblemBase
+    {
+        public override void Run()
+        {
+            int cnt = RunInternal(new DateTime(1901, 1, 1), new DateTime(1999, 12, 1));
+
+            Console.WriteLine(new DateTime(1900, 1, 1).DayOfWeek);
+            Console.WriteLine(cnt);
+        }
+
+        private int RunInternal(DateTime start, DateTime end)
+        {
+            int cnt = 0;
+
+            while (start <= end)
+            {
+                if (start.DayOfWeek == DayOfWeek.Monday) // looks like there is an error in task description. There should be Mondays not Sundays.
+                {
+                    cnt++;
+                }
+                start = start.AddMonths(1);
+            }
+
+            return cnt;
+        }
+    }
+}
