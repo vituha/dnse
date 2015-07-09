f04cf791c8f64051d53071a97212ba1159d026d5
spam@vituha.net <spam@vituha.net>
2015-07-09 04:51:13 +0300

    Problem 28 solved.

---
+++ ProjectEuler.Net/Problem/Problems/Problem28.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problem.Problems
+{
+    internal sealed class Problem28 : ProblemBase
+    {
+        public override void Run()
+        {
+            RunInternal(1001);
+        }
+
+        private void RunInternal(int max)
+        {
+            int sum = 1;
+            int adder = 1;
+            int round = 0;
+
+            while (++round * 2 < max)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    adder += round * 2;
+                    sum += adder;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
